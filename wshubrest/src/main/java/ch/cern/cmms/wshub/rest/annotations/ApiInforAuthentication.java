29c6d7b19480acd5f50c05db1d608886904042a6
Ricardo Almeida <ricardo.almeida@cern.ch>
2022-02-04 14:18:14 +0100

    Swagger: Password field is now obfuscated on the interface

--- wshubrest/src/main/java/ch/cern/cmms/wshub/rest/annotations/ApiInforAuthentication.java
+++ wshubrest/src/main/java/ch/cern/cmms/wshub/rest/annotations/ApiInforAuthentication.java
@@ -11,7 +11,7 @@ import java.lang.annotation.Target;
 
 @ApiImplicitParams({
         @ApiImplicitParam(name="INFOR_USER", value = "Username", required = true, dataType = "string", paramType = "header"),
-        @ApiImplicitParam(name="INFOR_PASSWORD", value = "Password", required = true, dataType = "string", paramType = "header"),
+        @ApiImplicitParam(name="INFOR_PASSWORD", value = "Password", required = true, dataType = "string", paramType = "header", format = "password"),
         @ApiImplicitParam(name="INFOR_TENANT", value = "Infor Tenant", dataType = "string", paramType = "header"),
         @ApiImplicitParam(name="INFOR_ORGANIZATION", value = "Infor Organization", defaultValue =  "*", dataType = "string", paramType = "header")
 })
