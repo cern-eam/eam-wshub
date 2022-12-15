b4bcb182d0c09347265b220dd24139d9ef43ae60
Lukasz Pater <Lukasz.Pater@cern.ch>
2022-12-15 08:37:47 +0100

    Add Equipment Reservations WS

---
+++ wshubrest/src/main/java/ch/cern/cmms/wshub/rest/controllers/EquipmentReservationController.java
@@ -0,0 +1,95 @@
+package ch.cern.cmms.wshub.rest.controllers;
+
+import ch.cern.cmms.wshub.rest.WSHubController;
+import ch.cern.cmms.wshub.rest.annotations.ApiInforAuthentication;
+import ch.cern.cmms.wshub.rest.annotations.ApiInforResponse;
+import ch.cern.cmms.wshub.rest.tools.AuthenticationTools;
+import ch.cern.eam.wshub.core.client.InforClient;
+import ch.cern.eam.wshub.core.services.equipment.entities.EquipmentReservation;
+import ch.cern.eam.wshub.core.tools.InforException;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+
+import javax.inject.Inject;
+import javax.ws.rs.*;
+import javax.ws.rs.core.Response;
+
+@Path("/equipmentreservations")
+@Api(tags={"Equipment Reservations"})
+public class EquipmentReservationController extends WSHubController {
+
+    @Inject
+    private AuthenticationTools authentication;
+
+    @Inject
+    private InforClient inforClient;
+
+    @POST
+    @Path("/")
+    @Produces("application/json")
+    @Consumes("application/json")
+    @ApiOperation("Create Equipment Reservation")
+    @ApiInforAuthentication
+    @ApiInforResponse
+    public Response createEquipmentReservation(EquipmentReservation equipmentReservation) {
+        try {
+            return ok(inforClient.getEquipmentReservationService().createEquipmentReservation(authentication.getInforContext(), equipmentReservation));
+        } catch (InforException e) {
+            return badRequest(e);
+        } catch(Exception e) {
+            return serverError(e);
+        }
+    }
+
+    @GET
+    @Path("/{code}")
+    @Produces("application/json")
+    @ApiOperation("Read Equipment Reservation")
+    @ApiInforAuthentication
+    @ApiInforResponse
+    public Response readEquipmentReservation(@PathParam("code") String code) {
+        try {
+            return ok(inforClient.getEquipmentReservationService().readEquipmentReservation(authentication.getInforContext(), code));
+        } catch (InforException e) {
+            return badRequest(e);
+        } catch(Exception e) {
+            return serverError(e);
+        }
+    }
+
+
+    @PUT
+    @Path("/{code}")
+    @Produces("application/json")
+    @Consumes("application/json")
+    @ApiOperation("Update Equipment Reservation")
+    @ApiInforAuthentication
+    @ApiInforResponse
+    public Response updateEquipmentReservation(@PathParam("code") String code, EquipmentReservation equipmentReservation) {
+        try {
+            equipmentReservation.setCode(code);
+            return ok(inforClient.getEquipmentReservationService().updateEquipmentReservation(authentication.getInforContext(), equipmentReservation));
+        } catch (InforException e) {
+            return badRequest(e);
+        } catch(Exception e) {
+            return serverError(e);
+        }
+    }
+
+    @DELETE
+    @Path("/{code}")
+    @Produces("application/json")
+    @ApiOperation("Delete Equipment Reservation")
+    @ApiInforAuthentication
+    @ApiInforResponse
+    public Response deleteEquipmentReservation(@PathParam("code") String code) {
+        try {
+            return ok(inforClient.getEquipmentReservationService().deleteEquipmentReservation(authentication.getInforContext(), code));
+        } catch (InforException e) {
+            return badRequest(e);
+        } catch(Exception e) {
+            return serverError(e);
+        }
+    }
+
+}
